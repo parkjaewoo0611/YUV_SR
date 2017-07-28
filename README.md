e30e51053ca108596efaef309cfc35dd901234c0
jaewoo <bjw0611@snu.ac.kr>
2017-07-28 13:29:49 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"YUV_420 to YUV_444 SR by subpixel shuffler" 
---
+++ main.py
@@ -0,0 +1,11 @@
+import model
+import tensorflow as tf
+import model
+import cv2
+#TODO: data input
+
+sess = tf.InteractiveSession()
+
+def main():
+    NN = model.CbCrSRNN()
+    cv2.imread()
